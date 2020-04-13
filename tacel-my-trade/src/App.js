4b0f3c216f15a2f1dc36190768ee53a8866026a1
doanmade <43007082+doanmade@users.noreply.github.com>
2020-04-13 03:33:57 -0500

    Got react to install properly, added dependencies, created folders, and created players.js seed file with all the players stats

---
+++ tacel-my-trade/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h2> Welcome To</h2>
+      <h1>Tackle My Trade</h1>
+    </div>
+  );
+}
+
+export default App;
